a6ff144741e66238a0ee44da1dfec4aa5015a090
Clint Ryan <clintryan3@gmail.com>
2016-11-23 21:08:00 +1100

    Adds linting to project

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
-#!/usr/bin/env node
-
-var kexec = require('kexec')
-var yargs = require('yargs')
-var path = require('path')
-var shell = require('shelljs')
-var resolve = require('resolve')
-var fs = require('fs')
+const kexec = require('kexec');
+const yargs = require('yargs');
+const path = require('path');
+const shell = require('shelljs');
+const resolve = require('resolve');
+const fs = require('fs');
 
 function getFileArgs() {
-  return yargs.argv._
+  return yargs.argv._;
 }
 
 function eslintNearFile(file) {
   if (file[0] !== '/') {
-    file = path.join(shell.pwd(), file)
+    file = path.join(shell.pwd(), file);
   }
 
-  var dir = shell.test('-d', file)
-    ? file
-    : path.dirname(file)
+  const dir = shell.test('-d', file) ? file : path.dirname(file);
 
   // resolve eslint
   try {
-    var resolvedLocation = resolve.sync('eslint/package.json', { basedir: dir })
-    var eslintPackageDir = path.dirname(resolvedLocation)
-    var eslintPackage = JSON.parse(fs.readFileSync(resolvedLocation))
-    return path.join(eslintPackageDir, eslintPackage.bin.eslint)
+    const resolvedLocation = resolve.sync('eslint/package.json', { basedir: dir });
+    const eslintPackageDir = path.dirname(resolvedLocation);
+    const eslintPackage = JSON.parse(fs.readFileSync(resolvedLocation));
+    return path.join(eslintPackageDir, eslintPackage.bin.eslint);
   } catch (err) {
-    shell.echo('could not find eslint near ' + file)
-    return shell.which('eslint')
+    shell.echo('could not find eslint near ' + file);
+    return shell.which('eslint');
   }
 }
 
 function main() {
-  var options = getFileArgs()
-  var relativeFrom = options.length === 0
-        ? shell.pwd()
-        : options[0]
-  var exe = eslintNearFile(relativeFrom)
+  const options = getFileArgs();
+  const relativeFrom = options.length === 0 ? shell.pwd() : options[0];
+  const exe = eslintNearFile(relativeFrom);
   if (exe) {
     // shell.echo("using eslint at " + exe)
-    kexec(exe, process.argv)
+    kexec(exe, process.argv.slice(2));
   } else {
-    shell.echo('eslint not found')
-    process.exit(1)
+    shell.echo('eslint not found');
+    process.exit(1);
   }
 }
 
-main()
+main();
